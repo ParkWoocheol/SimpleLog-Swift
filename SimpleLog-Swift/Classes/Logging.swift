64f69ea5d1902b13e1b2a21f2cf51fb053311704
Woocheol Park <magic1wp@naver.com>
2016-12-21 14:39:59 +0900

    added Logging function

---
+++ SimpleLog-Swift/Classes/Logging.swift
@@ -0,0 +1,67 @@
+//Copyright (c) 2016 Woocheol Park <admin@mrparkwc.com>
+//
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+//
+//The above copyright notice and this permission notice shall be included in
+//all copies or substantial portions of the Software.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//THE SOFTWARE.
+
+
+enum LogSeparator: String{
+    case DEBUG = "DEBUG"
+    case INFO = "INFO"
+    case WARN = "WARN"
+    case ERROR = "ERROR"
+}
+
+class Logging{
+    
+    static func d(message: String, function: String = #function, line: Int = #line){
+        logMessagePrint(separator: .DEBUG, logMessage: "FUNCTION: \(function), LINE: \(line), MESSAGE: \(message)")
+    }
+    
+    static func d(tag: String, message: String, line: Int = #line){
+        logMessagePrint(separator: .DEBUG, logMessage: "TAG: \(tag), LINE: \(line), MESSAGE: \(message)")
+    }
+    
+    static func i(message: String, function: String = #function, line: Int = #line){
+        logMessagePrint(separator: .INFO, logMessage: "FUNCTION: \(function), LINE: \(line), MESSAGE: \(message)")
+    }
+    
+    static func i(tag: String, message: String, line: Int = #line){
+        logMessagePrint(separator: .INFO, logMessage: "TAG: \(tag), LINE: \(line), MESSAGE: \(message)")
+    }
+    
+    static func w(message: String, function: String = #function, line: Int = #line){
+        logMessagePrint(separator: .WARN, logMessage: "FUNCTION: \(function), LINE: \(line), MESSAGE: \(message)")
+    }
+    
+    static func w(tag: String, message: String, line: Int = #line){
+        logMessagePrint(separator: .WARN, logMessage: "TAG: \(tag), LINE: \(line), MESSAGE: \(message)")
+    }
+    
+    static func e(message: String, function: String = #function, line: Int = #line){
+        logMessagePrint(separator: .ERROR, logMessage: "FUNCTION: \(function), LINE: \(line), MESSAGE: \(message)")
+    }
+    
+    static func e(tag: String, message: String, line: Int = #line){
+        logMessagePrint(separator: .ERROR, logMessage: "TAG: \(tag), LINE: \(line), MESSAGE: \(message)")
+    }
+    
+    static func logMessagePrint(separator: LogSeparator,logMessage: String){
+        print("| \(separator.rawValue) | \(logMessage)")
+    }
+    
+}
